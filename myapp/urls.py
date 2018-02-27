fafdd59accd02758021f91753c56a91ecfb01842
fans <fan.jinlong@qq.com>
2018-02-27 18:37:47 +0800

    评论数据库模型

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 	path('',views.index, name='index'),
 	# myapp/post/id
 	path('post/<int:pk>/', views.detail, name='detail'),
-	path('P<pk>[0-9]/$', views.detail, name='detail'),
-	path('archives/<year>/<month>/', views.archives, name='archives'),
-	path('category/P<pk>[0-9]/$', views.category, name='category'),
+	# path('P<pk>[0-9]/$', views.detail, name='detail'),
+	path('archives/<int:year>/<int:month>/', views.archives, name='archives'),
+	path('category/<int:pk>', views.category, name='category'),
 ]
